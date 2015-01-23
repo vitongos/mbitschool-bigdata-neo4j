96f8c803a7a1d05ea6a25d51044a38757aaaa3fd
Victor <vitongos@gmail.com>
2015-01-23 21:56:16 +0100

    Update Java 8.

--- deploy/java8.sh
+++ deploy/java8.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 cd ~/Downloads
-tar zxf jdk-8u25-linux-x64.tar.gz
+tar zxf jdk-8u31-linux-x64.tar.gz
 sudo mkdir /usr/local/java
-sudo mv jdk1.8.0_25/ /usr/local/java/
-sudo update-alternatives --install /usr/bin/java java /usr/local/java/jdk1.8.0_25/bin/java 100
+sudo mv jdk1.8.0_31/ /usr/local/java/
+sudo update-alternatives --install /usr/bin/java java /usr/local/java/jdk1.8.0_31/bin/java 100
 sudo update-alternatives --config java
-export JAVA_HOME=/usr/local/java/jdk1.8.0_25
+export JAVA_HOME=/usr/local/java/jdk1.8.0_31
 
 
 
