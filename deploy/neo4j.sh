2c83e7c73d7a1ed438f787b4298100b74e66f595
cloudera <vitongos@gmail.com>
2015-06-11 12:35:41 -0700

    Upgrade Neo4j.

--- deploy/neo4j.sh
+++ deploy/neo4j.sh
@@ -8,9 +8,17 @@ sudo mv neo4j-community-2.2.2/ /opt/neo4j/
 sudo ln -s /opt/neo4j/bin/neo4j /usr/bin/neo4j
 cd ~/neo4j-src/
 sudo sed -i -r 's/dbms\.security\.auth_enabled=true/dbms\.security\.auth_enabled=false/' /opt/neo4j/conf/neo4j-server.properties
+#neo4j start
+#neo4j stop
+mkdir /opt/neo4j/data/graph.db
+cp data/cineasts_12k_movies_50k_actors_2.1.6.zip /opt/neo4j/data/graph.db/
+cd /opt/neo4j/data/graph.db/
+unzip cineasts_12k_movies_50k_actors_2.1.6.zip
+mv cineasts_12k_movies_50k_actors.db/* ./
+rm -rf cineasts_12k_movies_50k_actors_2.1.6.zip cineasts_12k_movies_50k_actors.db
+#sudo sed -i -r 's/org\.neo4j\.server\.database\.location=data\/graph.db/org\.neo4j\.server\.database\.location=data\/cineasts.db/' /opt/neo4j/conf/neo4j-server.properties
+sudo sed -i -r 's/#allow_store_upgrade=true/allow_store_upgrade=true/' /opt/neo4j/conf/neo4j.properties
 neo4j start
-cd ~/neo4j-src/
-/opt/neo4j/bin/neo4j-shell -file data/insert.cyp
 
 
 
