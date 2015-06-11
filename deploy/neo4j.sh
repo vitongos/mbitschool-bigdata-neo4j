a5e518a513aef2e4d04b3ccb22cb2977c2cbbf5e
Victor <vitongos@gmail.com>
2015-06-11 20:54:16 +0200

    Update path.

--- deploy/neo4j.sh
+++ deploy/neo4j.sh
@@ -6,10 +6,10 @@ tar zxf neo4j.tar.gz
 rm -rf neo4j.tar.gz
 sudo mv neo4j-community-2.2.2/ /opt/neo4j/
 sudo ln -s /opt/neo4j/bin/neo4j /usr/bin/neo4j
-cd ~/use-case-src/
+cd ~/neo4j-src/
 sudo sed -i -r 's/dbms\.security\.auth_enabled=true/dbms\.security\.auth_enabled=false/' /opt/neo4j/conf/neo4j-server.properties
 neo4j start
-cd ~/use-case-src/
+cd ~/neo4j-src/
 /opt/neo4j/bin/neo4j-shell -file data/insert.cyp
 
 
