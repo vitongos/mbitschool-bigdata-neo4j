15ca9ebff529d3a995d7809310b748b6507ddd18
Victor <vitongos@gmail.com>
2015-06-11 20:46:08 +0200

    Upgrade Neo4j & Hava 8.

--- deploy/neo4j.sh
+++ deploy/neo4j.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 cd ~/Downloads
-wget -O neo4j.tar.gz http://neo4j.com/artifact.php?name=neo4j-community-2.1.6-unix.tar.gz
+wget -O neo4j.tar.gz http://neo4j.com/artifact.php?name=neo4j-community-2.2.2-unix.tar.gz
 tar zxf neo4j.tar.gz
-sudo mv neo4j-community-2.1.6/ /opt/neo4j/
+rm -rf neo4j.tar.gz
+sudo mv neo4j-community-2.2.2/ /opt/neo4j/
 sudo ln -s /opt/neo4j/bin/neo4j /usr/bin/neo4j
+cd ~/use-case-src/
+sudo sed -i -r 's/dbms\.security\.auth_enabled=true/dbms\.security\.auth_enabled=false/' /opt/neo4j/conf/neo4j-server.properties
+neo4j start
+cd ~/use-case-src/
+/opt/neo4j/bin/neo4j-shell -file data/insert.cyp
 
 
 
