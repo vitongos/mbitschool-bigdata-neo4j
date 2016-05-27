c253d5a0ddf5033361f2073474bb314cca2e0ee1
Victor <vitongos@gmail.com>
2016-05-27 20:47:06 +0200

    Upgrade software.

--- deploy/eclipse.sh
+++ deploy/eclipse.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd
-wget http://ftp-stud.fht-esslingen.de/pub/Mirrors/eclipse/technology/epp/downloads/release/luna/SR1/eclipse-java-luna-SR1-linux-gtk-x86_64.tar.gz
-sudo mv eclipse-java-luna-SR1-linux-gtk-x86_64.tar.gz /opt/
+wget http://ftp.fau.de/eclipse/technology/epp/downloads/release/mars/2/eclipse-jee-mars-2-linux-gtk-x86_64.tar.gz
+sudo mv eclipse-jee-mars-2-linux-gtk-x86_64.tar.gz /opt/
 cd /opt
-sudo tar xzf eclipse-java-luna-SR1-linux-gtk-x86_64.tar.gz
+sudo tar xzf eclipse-jee-mars-2-linux-gtk-x86_64.tar.gz
 sudo chown cloudera:cloudera eclipse -R
 sudo ln -s /opt/eclipse/eclipse /usr/bin/eclipse
 
--- deploy/java8.sh
+++ deploy/java8.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 cd ~/Downloads
-tar zxf jdk-8u45-linux-x64.tar.gz
+tar zxf jdk-8u92-linux-x64.tar.gz
 sudo mkdir /usr/local/java
-sudo mv jdk1.8.0_45/ /usr/local/java/
-sudo update-alternatives --install /usr/bin/java java /usr/local/java/jdk1.8.0_45/bin/java 100
+sudo mv jdk1.8.0_92/ /usr/local/java/
+sudo update-alternatives --install /usr/bin/java java /usr/local/java/jdk1.8.0_92/bin/java 100
 sudo update-alternatives --config java
-export JAVA_HOME=/usr/local/java/jdk1.8.0_45
+export JAVA_HOME=/usr/local/java/jdk1.8.0_92
 
 
 
