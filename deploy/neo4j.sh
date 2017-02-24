90967bf48225f1d81290c96cc09bbabbd6ff3978
Victor <vitongos@gmail.com>
2017-02-24 21:09:35 +0100

    Install Neo4j 3.1.1.

--- deploy/neo4j.sh
+++ deploy/neo4j.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd ~/Downloads
-wget -O neo4j.tar.gz http://neo4j.com/artifact.php?name=neo4j-community-2.3.3-unix.tar.gz
+wget -O neo4j.tar.gz http://neo4j.com/artifact.php?name=neo4j-community-3.1.1-unix.tar.gz
 tar zxf neo4j.tar.gz
 rm -rf neo4j.tar.gz
-sudo mv neo4j-community-2.3.3/ /opt/neo4j/
+sudo mv neo4j-community-3.1.1/ /opt/neo4j/
 sudo ln -s /opt/neo4j/bin/neo4j /usr/bin/neo4j
 cd ~/neo4j-src/
 sudo sed -i -r 's/dbms\.security\.auth_enabled=true/dbms\.security\.auth_enabled=false/' /opt/neo4j/conf/neo4j-server.properties
