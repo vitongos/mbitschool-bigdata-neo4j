e1c5e9da4691a1b3e11415d97690e3ae142f5126
Victor <vitongos@gmail.com>
2017-06-08 21:58:20 +0200

    Python packages setup.

--- deploy/python.sh
+++ deploy/python.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-yum install -y zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel
-cd /opt
-wget --no-check-certificate https://www.python.org/ftp/python/2.7.9/Python-2.7.9.tar.xz
-tar xf Python-2.7.9.tar.xz
-cd Python-2.7.9
-./configure --prefix=/usr/local
-make
-make altinstall
-ln -s /usr/local/bin/python2.7 /usr/local/bin/python
-
 cd /tmp
 curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 python get-pip.py
