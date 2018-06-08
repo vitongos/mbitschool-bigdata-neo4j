927c70f3004cc604d6abbe26daadc313ebf2fd1e
Victor Santiago <vitongos@users.noreply.github.com>
2018-06-08 14:54:25 +0200

    Update neo4j.sh

--- deploy/neo4j.sh
+++ deploy/neo4j.sh
@@ -3,6 +3,8 @@
 cd /tmp
 wget http://debian.neo4j.org/neotechnology.gpg.key
 rpm --import neotechnology.gpg.key
+wget http://example-data.neo4j.org/3.0-datasets/cineasts.tgz
+tar xzf cineasts.tgz
 
 cat <<EOF>  /etc/yum.repos.d/neo4j.repo
 [neo4j]
@@ -14,3 +16,10 @@ EOF
 
 yum install -y neo4j
 
+sed -i -r 's/#dbms\.security\.auth_enabled=false/dbms\.security\.auth_enabled=false/' /etc/neo4j/neo4j.conf
+sed -i -r 's/#dbms\.allow_upgrade=true/dbms\.allow_upgrade=true/' /etc/neo4j/neo4j.conf
+
+mv graph.db/* /var/lib/neo4j/data/databases/graph.db/
+chown neo4j:neo4j /var/lib/neo4j/data/databases/graph.db/ -R
+
+service neo4j start
