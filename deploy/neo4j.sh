abcf8b8b2d35953bb6a3175c944c4040b578a7cf
root <root@localhost.localdomain>
2018-06-13 18:24:35 +0200

    Update neo4j.

--- deploy/neo4j.sh
+++ deploy/neo4j.sh
@@ -19,7 +19,14 @@ yum install -y neo4j
 sed -i -r 's/#dbms\.security\.auth_enabled=false/dbms\.security\.auth_enabled=false/' /etc/neo4j/neo4j.conf
 sed -i -r 's/#dbms\.allow_upgrade=true/dbms\.allow_upgrade=true/' /etc/neo4j/neo4j.conf
 
-mv graph.db/* /var/lib/neo4j/data/databases/graph.db/
+service neo4j stop
+
+sleep 20
+
+service neo4j start
+
+mv /tmp/graph.db/* /var/lib/neo4j/data/databases/graph.db/
 chown neo4j:neo4j /var/lib/neo4j/data/databases/graph.db/ -R
 
 service neo4j start
+
