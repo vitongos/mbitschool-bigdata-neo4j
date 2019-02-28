882c2c2a72adf44cbc08cc8277a3a0c862bd212d
Victor Santiago <vitongos@users.noreply.github.com>
2019-02-28 20:39:38 +0100

    Delete eclipse.sh

--- deploy/eclipse.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /tmp
-wget http://ftp.fau.de/eclipse/technology/epp/downloads/release/mars/2/eclipse-jee-mars-2-linux-gtk-x86_64.tar.gz
-mv eclipse-jee-mars-2-linux-gtk-x86_64.tar.gz /opt/
-cd /opt
-tar xzf eclipse-jee-mars-2-linux-gtk-x86_64.tar.gz
-chown centos:centos eclipse -R
-ln -s /opt/eclipse/eclipse /usr/bin/eclipse
